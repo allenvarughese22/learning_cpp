82435de746504235f11dfe2944a2dd228915b2c1
allenvarughese22 <allenvrghs2007@gmail.com>
2021-12-02 18:58:20 +0530

    comparing array value

---
+++ ch7_ex4.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int size_list;
+    cout << "enter the number of elements you want to store in array" << endl;
+    cin >> size_list;
+    int list[size_list];
+    int maxint(int[], int);
+
+    int index = 0;
+    int largest_index;
+
+    // getting the values for the array
+    for (int i = 0; i < size_list; i++)
+    {
+        cout << "enter the number" << endl;
+        cin >> list[index];
+
+        index++;
+    }
+    // printing the array
+    for (int j = 0; j < size_list; j++)
+    {
+        cout << list[j] <<"  " <<ends;
+    }
+
+    largest_index = maxint(list, size_list);
+    cout << "the largest value: " << list[largest_index];
+    cout << "the largest value index: " << largest_index;
+}
+
+// function to calculate the index of largest value in an array
+int maxint(int list[], int size_array)
+{
+    int largest_value = list[0];
+    int largest_index = 0;
+    for (int k = 0; k < (size_array); k++)
+    {
+
+        if (list[k] > largest_value)
+        {
+            largest_value = list[k + 1];
+            largest_index = k;
+        }
+    }
+
+    // cout << "the largest value: " << largest_value;
+    // cout << "the largest value index: " << largest_index;
+    return largest_index;
+}
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,12 +1,2 @@
-#include <iostream>
-#include <iterator> // for std::size
-
-int main()
-{
-    char name[255] {}; // declare array large enough to hold 254 characters + null terminator
-    std::cout << "Enter your name: ";
-    std::cin.getline(name, std::size(name));
-    std::cout << "You entered: " << name << '\n';
-
-    return 0;
-}
+cout<<sizeof(list)<<endl;
+// cout<<(sizeof(list[0]))<<endl;
