3d11109552503ed1a92b17d51b083a1482a5d572
allenvarughese22 <allenvrghs2007@gmail.com>
2021-12-22 22:26:42 +0530

    prefix and postfix overloading on time

--- Robert Lafore Ch 1-7/swap.cpp
+++ Robert Lafore Ch 1-7/swap.cpp
@@ -1,22 +1,23 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-void swap(int&, int&);
-int a = 5, b = 10;
-cout << "the value of a is" <<a<<endl;
-cout<< "the value of b is" <<b<<endl;
+int main()
+{
+    void swap(int &, int &);
+    int a = 5, b = 10;
+    cout << "the value of a is" << a << endl;
+    cout << "the value of b is" << b << endl;
 
-swap(a,b);
+    swap(a, b);
 
-cout<< "______after swapping_______"<<endl;
-cout<< "the value of a is  " <<a<<endl;
-cout<< "the value of b is  " <<b<<endl;
+    cout << "______after swapping_______" << endl;
+    cout << "the value of a is  " << a << endl;
+    cout << "the value of b is  " << b << endl;
 }
 
-void swap(int& f, int& g){
-int temp = f;
-f = g;
-g = temp;
-
+void swap(int &f, int &g)
+{
+    int temp = f;
+    f = g;
+    g = temp;
 }
