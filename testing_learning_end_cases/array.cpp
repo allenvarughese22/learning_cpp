04250b0313670ab8f5ee14dcd31029c837126b4c
allenvarughese22 <allenvrghs2007@gmail.com>
2022-01-19 19:52:23 +0530

    day2_ds_practice

---
+++ testing_learning_end_cases/array.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    char ar[10];
+
+    cin >> ar;
+    char temp[10];
+
+    cout<<"printing"<<endl;
+    cout<< ar;
+    for ()
+    return 0;
+}
