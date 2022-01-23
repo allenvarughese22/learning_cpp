ffb6b12b8972984715cac9d84edec98f1c7cdbd6
allenvarughese22 <allenvrghs2007@gmail.com>
2022-01-23 23:51:22 +0530

    kadane_algo_naive.cpp

--- Data Structures and Algo/450_curated_list_Babbar/kadane's_algo_brute_python.py
+++ Data Structures and Algo/450_curated_list_Babbar/kadane's_algo_brute_python.py
@@ -2,15 +2,20 @@
 #code running but giving time out error in python
 #****************************
 
+"""Given an array Arr[] of N integers. Find the contiguous sub-array
+(containing at least one number) which
+has the maximum sum and return its sum."""
 #User function Template for python3
 
 class Solution:
     ##Complete this function
     #Function to find the sum of contiguous subarray with maximum sum.
     def maxSubArraySum(self,ar,N):
+        #initializing the array and index value
         s=[]
         start = 0
         end = 0
+        #brute summation
         for j in range(len(ar)):
             
             sum_ar=0
--- Data Structures and Algo/450_curated_list_Babbar/kadane's_algorithm.cpp
+++ Data Structures and Algo/450_curated_list_Babbar/kadane's_algorithm.cpp
@@ -1,23 +1,56 @@
-// Given an array Arr[] of N integers. 
-// Find the contiguous sub-array(containing at least one number) 
+// Given an array Arr[] of N integers.
+// Find the contiguous sub-array(containing at least one number)
 // which has the maximum sum and return its sum.
 
-
 // { Driver Code Starts
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-
- // } Driver Code Ends
-class Solution{
-    public:
+// } Driver Code Ends
+class Solution
+{
+public:
     // arr: input array
     // n: size of array
-    //Function to find the sum of contiguous subarray with maximum sum.
-    long long maxSubarraySum(int arr[], int n){
-        
-        // Your code here
-        
+    // Function to find the sum of contiguous subarray with maximum sum.
+    long long maxSubarraySum(int arr[], int n)
+    {
+
+        vector<int> summation_array;
+        int start = 0, end = 0;
+        int sum_loop_array = 0;
+
+        for (int j = 0; j < n; j++)
+        {
+            int sum_loop_array = 0;
+            for (int i = j; i < n; i++)
+            {
+                sum_loop_array += arr[i];
+                if (!summation_array.empty())
+                {
+                    int max_ele = *max_element(summation_array.begin(), summation_array.end());
+                    if (sum_loop_array > max_ele)
+                    {
+                        start = j;
+                        end = i;
+                    }
+                }
+                summation_array.push_back(sum_loop_array);
+            }
+        }
+
+        cout << "//" << endl;
+        cout << "start" << start << endl;
+        cout << "end" << end << endl;
+
+        cout << "//" << endl;
+        long long final_ans = 0;
+        for (int z = start; z <= end; z++)
+        {
+            final_ans += arr[z];
+        }
+        summation_array.clear();
+        return final_ans;
     }
 };
 
@@ -25,22 +58,22 @@ class Solution{
 
 int main()
 {
-    int t,n;
-    
-    cin>>t; //input testcases
-    while(t--) //while testcases exist
+    int t, n;
+
+    cin >> t;   // input testcases
+    while (t--) // while testcases exist
     {
-        
-        cin>>n; //input size of array
-        
+
+        cin >> n; // input size of array
+
         int a[n];
-        
-        for(int i=0;i<n;i++)
-            cin>>a[i]; //inputting elements of array
-            
+
+        for (int i = 0; i < n; i++)
+            cin >> a[i]; // inputting elements of array
+
         Solution ob;
-        
+
         cout << ob.maxSubarraySum(a, n) << endl;
     }
 }
-  // } Driver Code Ends
+// } Driver Code Ends
