968c27de3e3301e11ab60196b2e4ae14d58e0498
allenvarughese22 <allenvrghs2007@gmail.com>
2022-02-09 16:09:25 +0530

    spiral_matrix

--- Data Structures and Algo/450_curated_list_Babbar/spiraling_matrix.cpp
+++ Data Structures and Algo/450_curated_list_Babbar/spiraling_matrix.cpp
@@ -1,3 +1,5 @@
+// Given a matrix of size r*c. Traverse the matrix in spiral form.
+
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -5,47 +7,68 @@ using namespace std;
 
 int main()
 {
-    int r = 3, c = 3;
+    int r = 5, c = 4;
     vector<int> sp;
-    vector<vector<int>> matrix = {{1, 2, 3},
-                                  {4, 5, 6},
-                                  {7, 8, 9}};
+    
+    vector<vector<int>> matrix = 
+    {{22, 20, 29, 17},
+     {6, 21,  13, 19},
+     {17, 11, 3,  5},
+     {29, 27, 16, 21},
+     {10, 11, 21, 7}};
+
+    //  22 20 29 17
+    //  6 21  13 19
+ 
 
 
-
-// matrix[row last][c-1,c-2]
-
-    // for (int i = 0; i < r; i++)
-    // {
-    //     for (int j = 0; j < c; j++)
-    //     {
-
-    //         cout << matrix[0][j] << endl;
-    //     }
-    // }
-
-    //   matrix[row 1,2][c-1]
-    for (int j = 0; j < c; j++)
+    int f_m_s = r * c;
+    int z = 0;
+    while (sp.size() < f_m_s)
     {
+        for (int i = c - c + z; i <= c - 1 - z; i++) // first row({1, 2, 3, 4})(6,7)
+        {
 
-        // cout << matrix[0][j] << endl;
-        sp.push_back(matrix[0][j]);
+            // cout << matrix[0][j] << endl;
+            sp.push_back(matrix[z][i]);
+        }
+        if (sp.size() >= f_m_s)
+        {
+            break;
+        }
+
+        for (int i = 1 + z; i < r - z; i++) // first outer right(8,12,16) (11)
+        {
+            sp.push_back(matrix[i][c - 1 - z]);
+        }
+        if (sp.size() >= f_m_s)
+        {
+            break;
+        }
+        // matrix[r-1][c[1],c[0]]
+
+        for (int i = c - 2 - z; i >= 0 + z; i--) // first down (15,14,13)
+        {
+            sp.push_back(matrix[r - 1 - z][i]);
+        }
+        // matrix [ r-1 , r>0,i--]
+        if (sp.size() >= f_m_s)
+        {
+            break;
+        }
+        for (int i = r - 2 - z; i > 0+z; i--) // first outer left(9,5)
+        {
+            cout << "z" <<  z << endl;
+            sp.push_back(matrix[i][c - c + z]);
+        }
+
+        z++;
     }
-
-    for (int i = 1; i < r; i++)
-    {
-        sp.push_back(matrix[i][c - 1]);
-    }
-// matrix[r-1][c[1],c[0]]
-
-    for (int i = c-2; i >= 0; i--)
-    {
-        sp.push_back(matrix[r-1][i]);
-    }
-
+    cout << " " << endl;
     for (auto i : sp)
     {
-        cout << i;
+
+        cout << i << " ";
     }
 
     return 0;
