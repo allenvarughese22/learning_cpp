f5e856d5640a49e4cc0a74f92dc298711e672eb7
allenvarughese22 <allenvrghs2007@gmail.com>
2022-02-24 17:26:06 +0530

    linked_list_intro

---
+++ testing_learning_end_cases/linked_list_practice.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+};
+
+printlist(Node* n)
+{
+    while(n!=NULL)
+    {
+        cout<<n->data<<" ";
+        n = n->next;
+
+    }
+}
+
+int main()
+{
+    // Node *head = NULL;
+    // Node *second = NULL;
+    // Node *third = NULL;
+
+    Node * head = new Node();
+    Node * second = new Node();
+    Node * third = new Node();
+
+    head->data = 1;
+    head->next = second;
+    
+    second->data = 2;
+    second->next = third;
+
+    third->data = 3;
+    third->next  = NULL;
+
+    printlist(head);
+
+
+    return 0;
+}
